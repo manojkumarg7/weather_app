5984642cb3dce00e518c805c03d881310fc27b2e
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-03-07 13:23:15 +0530

    added api

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+const search = document.querySelector(".search-input");
 const search_btn = document.querySelector(".search-icon");
-const search_input = document.querySelector(".search-input");
-console.log(search_btn);
+const weather_image = document.querySelector(".weather_image");
+
+const apikey = "cffccf4fcd0b51c865fb330e17c02d73";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city) {
+  const res = await fetch(apiUrl + city + `&appid=${apikey}`);
+  const data = await res.json();
+  console.log(data);
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".celcius").innerHTML =
+    Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km / hr";
+
+  if (data.weather[0].main == "Clouds") {
+    weather_image.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Rain") {
+    weather_image.src = "images/rain.png";
+  } else if (data.weather[0].main == "Clear") {
+    weather_image.src = "images/Clear.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weather_image.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weather_image.src = "images/mist.png";
+  }
+}
+
 search_btn.addEventListener("click", () => {
-  search_input.focus();
-  console.log("focusing");
+  checkWeather(search.value);
+  console.log(search);
 });
